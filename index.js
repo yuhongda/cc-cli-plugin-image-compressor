eb69ea0b65ceb254c85a2e35bfdc887a00ae0f9e
yuhongda <silverage.y@gmail.com>
2018-06-15 11:11:41 +0800

    chain

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
-module.exports = (api, options) => {
+module.exports = (webpackConfig) => {
     const ImageCompressorPlugin = require('image-compressor-plugin');
 
-    api.chainWebpack(webpackConfig => {
-        webpackConfig
-            .plugin('image-compressor')
-            .use(ImageCompressorPlugin, [{
-                key: 'vmnT6A7UqOFjBONxBS2Z278yqE-HKPFD',
-                ext: ['png', 'jpeg', 'jpg']
-            }])
-    })
+    webpackConfig
+        .plugin('image-compressor')
+        .use(ImageCompressorPlugin, [{
+            key: 'vmnT6A7UqOFjBONxBS2Z278yqE-HKPFD',
+            ext: ['png', 'jpeg', 'jpg']
+        }])
 }
   
